4c855a30d3e5370793f1b7759ca765bec9a3c34c
denobytes <106289638+denobytes@users.noreply.github.com>
2023-06-14 22:42:16 -0500

    add popular option

--- cli.ts
+++ cli.ts
@@ -12,9 +12,11 @@ const args = argparse(Deno.args, {
     // output json
     "j",
     "json",
+
+    // output popular
+    "popular",
   ],
-  string: [
-  ],
+  string: [],
 });
 
 const commandName = `denolandx`;
@@ -27,15 +29,18 @@ Options:
   --help              Show this help message
 
   -j, --json          Output as json
+  --popular           Get 100 popular modules
 
   Examples:
   ${commandName} deno
   ${commandName} -j deno
+  ${commandName} --popular
 `;
 
 // parse args
 const help = args.help;
 let outputJSON = args.json || args.j;
+let outputPopular = args.popular;
 let searchterm = args._.at(0);
 let entries = [];
 
@@ -44,20 +49,40 @@ if (help) {
   Deno.exit();
 }
 
-// get
-let res = await fetch(`https://deno.land/x?query=${searchterm}`);
-let resText = await res.text();
+if (outputPopular) {
+  for (let pagenum in [1, 2, 3, 4, 5, 6]) {
+    // get
+    let res = await fetch(`https://deno.land/x?page=${pagenum}}`);
+    let resText = await res.text();
 
-// parse
-const doc = new DOMParser().parseFromString(resText, "text/html")!;
-let qEntries = await doc.querySelectorAll("li");
+    // parse
+    const doc = new DOMParser().parseFromString(resText, "text/html")!;
+    let qEntries = await doc.querySelectorAll("li");
 
-// process
-for (const qq of qEntries) {
-  let qt = qq.querySelector("div").querySelector("div");
-  let qd = qq.querySelector("div.col-span-2");
-  let entry = { "name": qt.textContent, "description": qd.textContent };
-  entries.push(entry);
+    // process
+    for (const qq of qEntries) {
+      let qt = qq.querySelector("div").querySelector("div");
+      let qd = qq.querySelector("div.col-span-2");
+      let entry = { "name": qt.textContent, "description": qd.textContent };
+      entries.push(entry);
+    }
+  }
+} else {
+  // get
+  let res = await fetch(`https://deno.land/x?query=${searchterm}`);
+  let resText = await res.text();
+
+  // parse
+  const doc = new DOMParser().parseFromString(resText, "text/html")!;
+  let qEntries = await doc.querySelectorAll("li");
+
+  // process
+  for (const qq of qEntries) {
+    let qt = qq.querySelector("div").querySelector("div");
+    let qd = qq.querySelector("div.col-span-2");
+    let entry = { "name": qt.textContent, "description": qd.textContent };
+    entries.push(entry);
+  }
 }
 
 // output
